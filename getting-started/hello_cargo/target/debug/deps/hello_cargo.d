6ec1090bb8a56985866ce146add13568b90ed5d7
krishna-kumar456 <krishnaqmar@gmail.com>
2020-08-18 22:11:21 +0530

    excluding unwanted files

--- getting-started/hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-D:\Code\rust\rust-book\getting-started\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-D:\Code\rust\rust-book\getting-started\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-src\main.rs:
--- getting-started/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-D:\Code\rust\rust-book\getting-started\hello_cargo\target\debug\hello_cargo.exe: D:\Code\rust\rust-book\getting-started\hello_cargo\src\main.rs
